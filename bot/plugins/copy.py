eb1a62988473b14b2e4c223a8b6f3077e298d8ca
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-09-24 15:40:12 +0600

    Update copy.py

--- bot/plugins/copy.py
+++ bot/plugins/copy.py
@@ -3,6 +3,7 @@ from bot.config import BotCommands, Messages
 from bot.helpers.utils import CustomFilters
 from bot.helpers.gdrive_utils import GoogleDrive
 from bot import LOGGER
+from bot import SUDO_USERS
 
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.Clone) & CustomFilters.auth_users & filters.user(SUDO_USERS))
 def _clone(client, message):
