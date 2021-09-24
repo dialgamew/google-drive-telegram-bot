dd487f39ad3e2f9b74513d5d7cfbef1cb857b098
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-09-24 15:39:29 +0600

    Update delete.py

--- bot/plugins/delete.py
+++ bot/plugins/delete.py
@@ -3,6 +3,7 @@ from bot.config import BotCommands, Messages
 from bot.helpers.gdrive_utils import GoogleDrive
 from bot.helpers.utils import CustomFilters
 from bot import LOGGER
+from bot import SUDO_USERS
 
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.Delete) & CustomFilters.auth_users & filters.user(SUDO_USERS))
 def _delete(client, message):
