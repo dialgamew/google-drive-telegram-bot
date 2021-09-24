75f7ad6538ddb7f9c14de713276c8a1722be3628
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-09-24 15:31:00 +0600

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -4,7 +4,7 @@ from bot.config import Messages as tr
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['start']), group=2)
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']) & filters.user(SUDO_USERS), group=2)
 def _start(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.START_MSG.format(message.from_user.mention),
@@ -12,7 +12,7 @@ def _start(client, message):
     )
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['help']), group=2)
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']) & filters.user(SUDO_USERS), group=2)
 def _help(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
@@ -42,7 +42,7 @@ def map(pos):
         button = [
             [
              InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK),
-             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/viperadnan-git/google-drive-telegram-bot/issues/new")
+             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/google-drive-telegram-bot/issues/new")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
